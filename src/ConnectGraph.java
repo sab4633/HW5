e3cc423678bb225448e3bcba0c3defc616fca7e6
Sofia Bzhilyanskaya <sab4633@rit.edu>
2021-11-05 14:30:44 -0400

    trying soemthing

--- src/ConnectGraph.java
+++ src/ConnectGraph.java
@@ -1,44 +1,47 @@
 import java.util.Scanner;
 
 public class ConnectGraph {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); //number of matrices
-        LinkedList[] adjacencyList = new LinkedList[n+1];
+        LinkedList[] adjacencyList = new LinkedList[n + 1];
         int m = sc.nextInt();
-        for (int i =0; i < m; i++) { //iterate through input placing each matrix into the array;
-            int v1 = sc.nextInt();
-            int v2 = sc.nextInt();
-            if(adjacencyList[v1] ==null){
-                adjacencyList[v1] = new LinkedList(v2);
-            }else{
-                adjacencyList[v1].add(v2);
-            }
-            if(adjacencyList[v2] ==null){
-                adjacencyList[v2] = new LinkedList(v1);
-            }else{
-                adjacencyList[v2].add(v1);
-            }
-        }
-        ConnectGraph program = new ConnectGraph();
-        boolean[] visited = new boolean[n+1];
-        int roads = -1;
-        int[] DFS = new int[n+m+1];
-        int top = -1;
-        for(int i =1; i<=n; i++){
-            visited[i] = false;
-        }
-        int count = 0;
-        for(int i =1; i<=n; i++){
-            if(visited[i] == false){
-                roads++;
-                program.DFS(adjacencyList, visited, i, count);
-                if(count==n){
-                    break;
+        if (m > n * 2) {
+            System.out.println(0);
+        } else {
+            for (int i = 0; i < m; i++) { //iterate through input placing each matrix into the array;
+                int v1 = sc.nextInt();
+                int v2 = sc.nextInt();
+                if (adjacencyList[v1] == null) {
+                    adjacencyList[v1] = new LinkedList(v2);
+                } else {
+                    adjacencyList[v1].add(v2);
+                }
+                if (adjacencyList[v2] == null) {
+                    adjacencyList[v2] = new LinkedList(v1);
+                } else {
+                    adjacencyList[v2].add(v1);
                 }
             }
+            ConnectGraph program = new ConnectGraph();
+            boolean[] visited = new boolean[n + 1];
+            int roads = -1;
+            int[] DFS = new int[n + m + 1];
+            int top = -1;
+            for (int i = 1; i <= n; i++) {
+                visited[i] = false;
+            }
+            int count = 0;
+            for (int i = 1; i <= n; i++) {
+                if (visited[i] == false) {
+                    roads++;
+                    program.DFS(adjacencyList, visited, i, count);
+                    if (count == n) {
+                        break;
+                    }
+                }
 
-        }
+            }
 //        for(int i =1; i<=n; i++){
 //            if(visited[i] ==false){
 //                roads++;
@@ -62,8 +65,11 @@ public class ConnectGraph {
 //            }
 //
 //        }
-        System.out.println(roads);
+            System.out.println(roads);
+
+        }
     }
+
     public void DFS(LinkedList[] adjacencyList, boolean[] visited, int value, int count){
         visited[value] = true;
         LinkedList temp = adjacencyList[value];
